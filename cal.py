3271d4b6301ddbaec963dcc2190b44dd35e8bdf8
Khadija <khadijaflem14@gmail.com>
2024-03-18 10:48:02 -0400

    cal.py
    This program uses a list of minutes (10, 15, 20, 25, and 30) and a for loop to iterate over each minute. It multiplies the number of minutes by the calories per minute to calculate the total calories burned, and then prints the result to the console using f-strings.

---
+++ cal.py
@@ -0,0 +1,14 @@
+calories_per_minute = 4.2
+
+for minute in [10, 15, 20, 25, 30]:
+    calories_burned = 10 * calories_per_minute
+print(f"After {minute} minutes, you burned {calories_burned} calories.")
+for minute in [10, 15, 20, 25, 30]:
+    calories_burned = 20 * calories_per_minute
+print(f"After {minute} minutes, you burned {calories_burned} calories.")
+for minute in [10, 15, 20, 25, 30]:
+    calories_burned = 25 * calories_per_minute
+print(f"After {minute} minutes, you burned {calories_burned} calories.")
+for minute in [10, 15, 20, 25, 30]:
+    calories_burned = 30 * calories_per_minute
+print(f"After {minute} minutes, you burned {calories_burned} calories.")
